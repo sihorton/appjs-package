5e36513f77030b70d842cdfcc72c25f5953076d9
Simon Horton <sij.horton@gmail.com>
2012-11-06 16:16:27 +0100

    Returned a router and readPackageFile function if package file is detected.

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ var Me = {
 		,preferOfficialModules:false
 		,moduleDir:__dirname+"/node_modules/"
 	},
-	path:null
-	,getPackageInfo:function(uri, callBack) {
+	getPackageInfo:function(uri, callBack) {
 		var path=require('path')
 			fs=require('fs')
 		;
@@ -17,17 +16,13 @@ var Me = {
 		};
 		fs.stat(pInfo.path, function(err, stat) {
 			if(err) return callBack(err,pInfo);
-			console.log("stat ok");
-			//is it a package.
+			var errTxt = '';
 			if (stat.isDirectory()) {
 				pInfo.isPackage = false;
 			}
 			if (stat.isFile()) {
-				var ext = path.extname(pInfo.path||'');
-				
-				console.log("file ext=",ext);
 				switch(path.extname(pInfo.path||'')) {
-					case '.appjs':
+					case Me.config.packageExt:
 						pInfo.isPackage = true;
 					break;
 					default:
@@ -35,8 +30,46 @@ var Me = {
 					break;
 				}
 			}
-			callBack(null,pInfo);
+			if (pInfo.isPackage) {
+				var mime = require('mime'),
+				AdmZip = require('adm-zip');
+				var packagedApp = new AdmZip(pInfo.path);
+				pInfo.router = function router(request, response, next){
+					if (request.method === 'get') {
+						var url = request.pathname === '/' ? '/index.html' : request.pathname;
+						if (packagedApp && packagedApp.getEntry('content/'+url.substring(1))) {
+							var mimetype = mime.lookup(url);
+							packagedApp.readFileAsync('content/'+url.substring(1),function(buffer,err) {
+								if (err) {
+									response.send(500,'text/plain',new Buffer("500: Internal Server Error\n"+err, "utf-8"));
+								} else {
+									response.send(200,mimetype,buffer);
+								}
+							});		
+						} else {
+							next();
+						}
+					} else {
+						next();
+					}
+				};
+				pInfo.readPackageFile = function(filename,callback) {
+					fs.readFile(path.resolve(pInfo.path,filename),callback);
+				}
+				pInfo._package = packagedApp;
+				Me.checkDependancies(pInfo, callBack);
+			} else {
+				callBack(errTxt,pInfo);
+			}
 		});
+	},checkDependancies:function(pInfo, callBack) {
+		if (pInfo._package.getEntry(Me.config.appInfoFile)) {
+			//package.json contains dependancies.
+			console.log('reading app package.json');
+		} else {
+			
+			callBack(errTxt,pInfo);
+		}
 	}
 }
 module.exports = Me;
