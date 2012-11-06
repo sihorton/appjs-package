65dd7a29759120ead33763b07ab113f1592f5912
Simon Horton <sij.horton@gmail.com>
2012-11-06 15:40:23 +0100

    Implemented simple package detection and data return to callback.

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
-module.exports = function packagedAppRouter(root) {
-	try {
-		var packagedApp = new AdmZip(root);
-	} catch(e) {
-		throw (e);
-		//return function router(request, response, next) { next(); };
-	}
-	return function router(request, response, next){
-		if (request.method === 'get') {
-			var url = request.pathname === '/' ? '/index.html' : request.pathname;
-			if (packagedApp && packagedApp.getEntry('content/'+url.substring(1))) {
-				var mimetype = mime.lookup(url);
-				packagedApp.readFileAsync('content/'+url.substring(1),function(buffer,err) {
-					if (err) {
-						response.send(500,'text/plain',new Buffer("500: Internal Server Error\n"+err, "utf-8"));
-					} else {
-						response.send(200,mimetype,buffer);
-					}
-				});		
-			} else {
-				next();
+
+var Me = {
+	config: {
+		packageExt:'.appjs'
+		,modulePackageExt:'.modpack'
+		,appInfoFile:'package.json'
+		,preferOfficialModules:false
+		,moduleDir:__dirname+"/node_modules/"
+	},
+	path:null
+	,getPackageInfo:function(uri, callBack) {
+		var path=require('path')
+			fs=require('fs')
+		;
+		var pInfo = {
+			path:path.resolve(uri)
+		};
+		fs.stat(pInfo.path, function(err, stat) {
+			if(err) return callBack(err,pInfo);
+			console.log("stat ok");
+			//is it a package.
+			if (stat.isDirectory()) {
+				pInfo.isPackage = false;
 			}
-		} else {
-			next();
-		}
-	};
+			if (stat.isFile()) {
+				var ext = path.extname(pInfo.path||'');
+				
+				console.log("file ext=",ext);
+				switch(path.extname(pInfo.path||'')) {
+					case '.appjs':
+						pInfo.isPackage = true;
+					break;
+					default:
+						pInfo.isPackage = false;
+					break;
+				}
+			}
+			callBack(null,pInfo);
+		});
+	}
 }
+module.exports = Me;
